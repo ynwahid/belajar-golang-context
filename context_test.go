7574e49958ca67cf3cf2d261dbeba639efa429d6
ynwahid <yusufedu99@gmail.com>
2025-08-25 08:27:09 +0700

    context

---
+++ context_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
