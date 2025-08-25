32cf0b3b55babaf7b9d7e132bd5707f5b0324e62
ynwahid <yusufedu99@gmail.com>
2025-08-25 10:02:46 +0700

    context with cancel

--- context_test.go
+++ context_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -61,4 +63,45 @@ func TestContextGetValue(t *testing.T) {
 	fmt.Println(contextF.Value("c"))
 	fmt.Println(contextF.Value("b"))
 	fmt.Println(contextA.Value("b"))
+}
+
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutines:", runtime.NumGoroutine())
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	destination := CreateCounter(ctx)
+	fmt.Println("Total Goroutines:", runtime.NumGoroutine())
+	for n := range destination {
+		fmt.Println("Counter:", n)
+
+		if n == 10 {
+			break
+		}
+	}
+	cancel()
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Total Goroutines:", runtime.NumGoroutine())
 }
