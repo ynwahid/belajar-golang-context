54a5f073954e3b94c28096d142647fa231a1494b
ynwahid <yusufedu99@gmail.com>
2025-08-25 09:40:28 +0700

    get context value

--- context_test.go
+++ context_test.go
@@ -35,3 +35,30 @@ func TestContextWithValue(t *testing.T) {
 	fmt.Println(contextF)
 	fmt.Println(contextG)
 }
+
+func TestContextGetValue(t *testing.T) {
+	contextA := context.Background()
+
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "c")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	contextG := context.WithValue(contextF, "g", "G")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+	fmt.Println(contextG)
+
+	fmt.Println(contextF.Value("f"))
+	fmt.Println(contextF.Value("c"))
+	fmt.Println(contextF.Value("b"))
+	fmt.Println(contextA.Value("b"))
+}
